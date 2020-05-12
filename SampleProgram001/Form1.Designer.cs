009aee5cd0fe45083add49cab8b56e18058b3f10
高　誠晩 <33561581+sungmanko@users.noreply.github.com>
2020-05-12 09:52:33 +0900

    Add files via upload

---
+++ SampleProgram001/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SampleProgram001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn04 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btn05 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn04
+            // 
+            this.btn04.Location = new System.Drawing.Point(118, 10);
+            this.btn04.Name = "btn04";
+            this.btn04.Size = new System.Drawing.Size(340, 23);
+            this.btn04.TabIndex = 0;
+            this.btn04.Text = "04. 태어난년도 입력 & 띠 출력";
+            this.btn04.UseVisualStyleBackColor = true;
+            this.btn04.Click += new System.EventHandler(this.btn04_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 84);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // btn05
+            // 
+            this.btn05.Location = new System.Drawing.Point(118, 82);
+            this.btn05.Name = "btn05";
+            this.btn05.Size = new System.Drawing.Size(340, 23);
+            this.btn05.TabIndex = 3;
+            this.btn05.Text = "05. 사용자에게 월을 입력받아 계절을 출력";
+            this.btn05.UseVisualStyleBackColor = true;
+            this.btn05.Click += new System.EventHandler(this.btn05_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(340, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "06. switch 문으로";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn05);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn04);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn04;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btn05;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SampleProgram001/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace SampleProgram001
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btn04_Click(object sender, EventArgs e)
+        {
+            ArrayList al = new ArrayList();
+            al.Add("원숭이");
+            al.Add("닭");
+            al.Add("개");
+            al.Add("돼지");
+            al.Add("쥐");
+            al.Add("소");
+            al.Add("범");
+            al.Add("토끼");
+            al.Add("용");
+            al.Add("뱀");
+            al.Add("말");
+            al.Add("양");
+
+            MessageBox.Show(al[Convert.ToInt32(comboBox1.SelectedIndex)].ToString());
+        }
+
+        private void btn05_Click(object sender, EventArgs e)
+        {
+            string result = string.Empty;
+            switch(comboBox2.SelectedIndex)
+            {
+                case 11:
+                case 0:
+                case 1:
+                    result = "겨울";
+                    break;
+
+                case 2:
+                case 3:
+                case 4:
+                    result = "봄";
+                    break;
+
+                case 5:
+                case 6:
+                case 7:
+                    result = "여름";
+                    break;
+
+                case 8:
+                case 9:
+                case 10:
+                    result = "가을";
+                    break;
+            }
+
+            MessageBox.Show(result);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+            string defaultText1 = "수강해야 하는 전공 학점:";
+            string defaultText2 = "학점";
+
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine(defaultText1 + "12" + defaultText2);
+                    break;
+                case 2:
+                    Console.WriteLine(defaultText1 + "18" + defaultText2);
+                    break;
+                case 3:
+                    Console.WriteLine(defaultText1 + "10" + defaultText2);
+                    break;
+                case 4:
+                    Console.WriteLine(defaultText1 + "18" + defaultText2);
+                    break;
+                default:
+                    // 기본값을 아무것도 출력하지 않는다.
+                    break;
+            }
+        }
+    }
+}
